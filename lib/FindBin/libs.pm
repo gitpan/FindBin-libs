2dc5ef8cdfb20184a568245d09e95059c73202f7
Steven Lembark <lembark@wrkhors.com>
2004-03-22 17:10:49 +0000

    Import of LEMBARK/FindBin-libs-1.00 from CPAN.
    gitpan-cpan-distribution: FindBin-libs
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         LEMBARK/FindBin-libs-1.00.tar.gz
    gitpan-cpan-author:       LEMBARK
    gitpan-cpan-maturity:     released

--- lib/FindBin/libs.pm
+++ lib/FindBin/libs.pm
@@ -37,7 +37,7 @@ use Cwd qw( &abs_path );
 # package variables 
 ########################################################################
 
-our $VERSION = '0.14';
+our $VERSION = '1.00';
 
 my %defaultz = 
 (
